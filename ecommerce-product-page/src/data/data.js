7ce6bcd281e129dfc6add1ae8834d6e0d3b26529
Levent KÖYBAŞI <leventkoybasi@hotmail.com>
2024-06-07 17:39:57 -0700

    update data Js

--- ecommerce-product-page/src/data/data.js
+++ ecommerce-product-page/src/data/data.js
@@ -1,22 +1,31 @@
+import imageProduct1 from "../assets/image-product-1.jpg";
+import imageProduct1Thumbnail from "../assets/image-product-1-thumbnail.jpg";
+import imageProduct2 from "../assets/image-product-2.jpg";
+import imageProduct2Thumbnail from "../assets/image-product-2-thumbnail.jpg";
+import imageProduct3 from "../assets/image-product-3.jpg";
+import imageProduct3Thumbnail from "../assets/image-product-3-thumbnail.jpg";
+import imageProduct4 from "../assets/image-product-4.jpg";
+import imageProduct4Thumbnail from "../assets/image-product-4-thumbnail.jpg";
+
 export const data = [
   {
     id: 1,
-    mainImage: "../src/assets/image-product-1.jpg",
-    thumbnail: "../src/assets/image-product-1-thumbnail.jpg",
+    mainImage: imageProduct1,
+    thumbnail: imageProduct1Thumbnail,
   },
   {
     id: 2,
-    mainImage: "../src/assets/image-product-2.jpg",
-    thumbnail: "../src/assets/image-product-2-thumbnail.jpg",
+    mainImage: imageProduct2,
+    thumbnail: imageProduct2Thumbnail,
   },
   {
     id: 3,
-    mainImage: "../src/assets/image-product-3.jpg",
-    thumbnail: "../src/assets/image-product-3-thumbnail.jpg",
+    mainImage: imageProduct3,
+    thumbnail: imageProduct3Thumbnail,
   },
   {
     id: 4,
-    mainImage: "../src/assets/image-product-4.jpg",
-    thumbnail: "../src/assets/image-product-4-thumbnail.jpg",
+    mainImage: imageProduct4,
+    thumbnail: imageProduct4Thumbnail,
   },
 ];
